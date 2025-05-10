7b21b8ca92efea830709e229c3a23afaf1aa1008
Victor <psygreg@pm.me>
2025-05-10 07:34:05 -0300

    better way of doing things

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Debian repacking of FireAlpaca's AppImage, because AppImages are just not that great.
 
+Requires `git`, which you can install running `sudo apt install git`.
+
 ## Usage
 
 I made an installer script to fix .desktop files not showing up properly in the Applications menu after installing the .deb package.
 
-- Download both `FireAlpaca.deb` and `installer.sh` from Releases.
-- Open a Terminal in the folder you downloaded those files, then:\
+`git clone https://github.com/psygreg/firealpaca-deb.git`\
+`cd firealpaca-deb`\
 `chmod +x installer.sh`\
 `./installer.sh`
--- installer.sh
+++ installer.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 # installation script to fix .desktop files
-sudo dpkg -i FireAlpaca.deb;
-sudo sed -i '5 s|.*|Exec=/opt/FireAlpaca/usr/bin/FireAlpaca|' /opt/FireAlpaca/FireAlpaca.desktop;
-sudo sed -i '5 s|.*|Exec=/opt/FireAlpaca/usr/bin/FireAlpaca|' /usr/share/applications/FireAlpaca.desktop;
-sudo sed -i '6 s|.*|Icon=fa|' /usr/share/applications/FireAlpaca.desktop;
+# functions
+install () {
+    wget https://github.com/psygreg/firealpaca-deb/releases/download/2.13.14/FireAlpaca.deb;
+    sudo dpkg -i FireAlpaca.deb;
+    sudo sed -i '5 s|.*|Exec=/opt/FireAlpaca/usr/bin/FireAlpaca|' /opt/FireAlpaca/FireAlpaca.desktop;
+    sudo sed -i '5 s|.*|Exec=/opt/FireAlpaca/usr/bin/FireAlpaca|' /usr/share/applications/FireAlpaca.desktop;
+    sudo sed -i '6 s|.*|Icon=fa|' /usr/share/applications/FireAlpaca.desktop;
+}
+# runtime
+. /etc/os-release
+if [ "$ID_LIKE" == "debian" ]; then
+    if command -v wget >/dev/null 2>&1; then
+        install
+        echo "All done."
+        sleep 3
+        exit 0
+    else
+        sudo apt install wget
+        install
+        echo "All done."
+        sleep 3
+        exit 0
+    fi
+else
+    echo "Invalid operating system."
+    sleep 3
+    exit 1
+fi
+
 
